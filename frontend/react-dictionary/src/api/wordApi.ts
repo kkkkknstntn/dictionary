9500a983749681c8e02b0f313a18e1505e0eff2b
Mardvey-UMA <chikernut213@gmail.com>
2025-05-11 12:14:23 +0400

    initial commit

--- frontend/react-dictionary/src/api/wordApi.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:8080",
-});
-
-export const addWord = (data: FormData) => api.post("/api/words", data);
-export const getWords = () => api.get("/api/words");
